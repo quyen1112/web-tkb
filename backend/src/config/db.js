2374d17b6df5c95467f46fcb5711650a54a3e503
quyen1112 <phanvanquyen1112@gmail.com>
2026-04-26 17:06:39 +0700

    fix connect render database

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -6,11 +6,10 @@ if (!process.env.JWT_SECRET) {
 }
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 pool.on('error', (err) => {
